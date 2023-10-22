2487d819a3b5bf3a4abe1df9c58c259134a2f658
Broque Thomas <batmanesignal@gmail.com>
2023-10-22 16:20:44 -0700

    update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import MobileSidebar from "./mobile-sidebar";
 import { get } from "http";
 import { getApiLimitCount } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
+import Update from "./update";
 
 
 const Navbar = async () => {
@@ -13,6 +14,7 @@ const Navbar = async () => {
     return (
         <div className="flex items-center p-4">
             <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+            <Update />
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/" />
             </div>
